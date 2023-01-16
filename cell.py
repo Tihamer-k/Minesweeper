d9c8828ef9217519efa259784da3280b2f80d944
Tihamer-K <tihameraldana@icloud.com>
2023-01-15 19:05:44 -0500

    showing if mines are near or not, methods modified

--- cell.py
+++ cell.py
@@ -26,7 +26,7 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f"{self.x},{self.y}"
+            # text=f"{self.x},{self.y}"
         )
         btn.bind('<Button-1>', self.left_click_actions)
         btn.bind('<Button-3>', self.right_click_actions)
@@ -39,9 +39,8 @@ class Cell:
         else:
             self.show_cell()
 
-    def show_cell(self):
-        # print(self.get_cell_by_axis(0,0))
-        surrounded_cells = [
+    def surrounded_cells(self):
+        cells = [
             get_cell_by_axis(self.x - 1, self.y - 1),
             get_cell_by_axis(self.x - 1, self.y),
             get_cell_by_axis(self.x - 1, self.y + 1),
@@ -52,10 +51,23 @@ class Cell:
             get_cell_by_axis(self.x, self.y + 1)
         ]
 
-        surrounded_cells = [cell for cell in surrounded_cells if cell is not None]
-        print(surrounded_cells)
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    @property
+    def surrounded_cells_length(self):
+        count = 0
+        for cell in self.surrounded_cells():
+            if cell.is_mine:
+                count += 1
+        return count
+
+    def show_cell(self):
+        print("Hay ", self.surrounded_cells_length, " mina/s por acá")
+        self.cell_btn_obj.configure(text=self.surrounded_cells_length)
 
     def show_mine(self):
+        print("¡Booooom!")
         self.cell_btn_obj.configure(bg="red")
 
     @staticmethod
